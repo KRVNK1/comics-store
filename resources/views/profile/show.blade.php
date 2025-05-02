caf4bc7e8b003072cd9ca1014bc36a62e29bff65
Курашов Никита Алексеевич <22065@iat.iat>
2025-05-02 13:54:24 +0800

    xdf

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -162,7 +162,7 @@
                                     <td>{{ $order->created_at->format('d-m-Y') }}</td>
                                     <td class="price-cell">{{ number_format($item->price * $item->quantity, 2, '.', '') }}</td>
                                     <td>
-                                        <a href="{{ route('cart.add-again', ['product' => $item->product->id, 'quantity' => $item->quantity]) }}" class="reorder-button">
+                                        <a href="" class="reorder-button">
                                             Re-Order
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                 <path d="M5 12h14"></path>
